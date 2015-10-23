2c2d5c61d33c494cc162b9af0d03b8d90934bb3e
Lukas Matt <lukas@zauberstuhl.de>
2015-10-23 20:59:06 +0200

    Add custom gui interface for AB

---
+++ ab_gui_interface.h
@@ -0,0 +1,28 @@
+/*
+ * NodeJS wrapper-plugin for Aqbanking
+ * Copyright (C) 2015  Lukas Matt <lukas@zauberstuhl.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ABGUIINTERFACE_H
+#define ABGUIINTERFACE_H
+
+int CUSTOM_GETPASSWORD_FN (GWEN_GUI *, unsigned int flags, const char * token,
+                           const char * title, const char * text, char * buffer,
+                           int minLen, int maxLen, unsigned int id);
+
+int CUSTOM_CHECKCERT_FN (GWEN_GUI *, const GWEN_SSLCERTDESCR * cd, GWEN_SYNCIO * sio, unsigned int id);
+
+#endif
